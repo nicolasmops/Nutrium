d4ebc99ed7d09b1f59fb41d530c72745ed25c839
nicolasmops <mopsnicolas@gmail.com>
2026-09-26 12:22:11 -0300

    casi termino el primer popup

--- frontend/scanner/home/script.js
+++ frontend/scanner/home/script.js
@@ -6,3 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }); 
   console.log("se commitear y pushear con cmd")
+
+let popup = document.getElementById("popup");
+let puedeONo = false;
+
+if(puedeONo == true){
+  popup.showModal()
+}
+else{
+  popup.close()
+}
+
+let siButton = document.getElementById("si");
+let noButton = document.getElementById("no");
+síButton.addEventListener("click", funcionGuardar)
+noButton.addEventListener("click", funcionCerrar)
+
+function funcionGuardar(){
+  popup.close();
+  popupGuardar.showModal();
+}
+function funcionCerrar(){
+  popup.close();
+}
